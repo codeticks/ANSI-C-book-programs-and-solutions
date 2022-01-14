db195dad4c5d7a5fb3216d6e149736a7cde7e1cf
codeticks <neutron_stays@protonmail.com>
2022-01-15 01:32:32 +0530

    Day 3 Started

---
+++ Day 3 15.01.2022/SwappingValues.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#define x 3
+#define y 6
+#define z 8
+int main(){
+
+
+printf("The value of x,y,z are %d %d %d",x,y,z);
+
+// x = y;
+// y = z;
+// z = x;
+
+// printf("\nNow after swapping the values of x,y and z , the values are : %d %d %d",x,y,z);
+
+return 0;
+}
+
+//this isn't working because i think that making the thigs defined is giving us error cz now we can't change them in futher expressions.
+
+//will try something new tommrow.
